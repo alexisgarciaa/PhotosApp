e8bcd336afa2298d35f89878125f7e0e085f98cf
bryan garcia <bryan.garcia@novatechdev.com>
2022-10-30 11:23:51 -0600

    Initial Commit

---
+++ PhotosApp/PhotosAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotosAppApp.swift
+//  PhotosApp
+//
+//  Created by Alexis on 30/10/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotosAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
